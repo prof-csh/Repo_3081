96208a6cf7a7d9bc82b411344a4da007fd9c8d6d
KadicAjas96 <55855746+KadicAjas96@users.noreply.github.com>
2019-11-30 16:19:03 -0800

    updated markers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,46 +8,32 @@ import { MouseEvent } from '@agm/core';
 })
 export class AppComponent  {
   // google maps zoom level
-  zoom: number = 8;
+  zoom: number = 12;
   
   // initial center position for the map
   lat: number = 47.608013;
   lng: number = -122.335167;
 
-  clickedMarker(label: string, index: number) {
-    console.log(`clicked the marker: ${label || index}`)
-  }
-  
-  mapClicked($event: MouseEvent) {
-    this.markers.push({
-      lat: $event.coords.lat,
-      lng: $event.coords.lng,
-      draggable: true
-    });
-  }
-  
-  markerDragEnd(m: marker, $event: MouseEvent) {
-    console.log('dragEnd', m, $event);
-  }
-  
+ 
   markers: marker[] = [
 	  {
-		  lat: 47.5964,
-		  lng: -122.3840,
+		  lat: 47.6101359,
+		  lng: -122.3420567,
 		  label: 'A',
 		  draggable: false
 	  },
 	  {
-		  lat: 51.373858,
-		  lng: 7.215982,
+		  lat: 47.5947,
+		  lng: 	-122.3844,
 		  label: 'B',
 		  draggable: false
 	  },
 	  {
-		  lat: 51.723858,
-		  lng: 7.895982,
+		  lat: 47.620422,
+		  lng: -122.349358
+,
 		  label: 'C',
-		  draggable: true
+		  draggable: false
 	  }
   ]
 }
